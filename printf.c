1d4cfe4d4609e178d64765854d515ae0f3962a05
Sadiya <sadiatusuf@yahoo.com>
2023-10-17 08:48:51 -0700

    task o

--- printf.c
+++ printf.c
@@ -1,18 +1,72 @@
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
-#include <stdio.h>
+
+/**
+ * _write_char - Write a character to the standard output
+ * @c: The character to write
+ * @count: Pointer to the character count
+ */
+void _write_char(char c, int *count)
+{
+	write(1, &c, 1);
+	(*count)++;
+}
+
+/**
+ * _write_str - Write a string to the standard output
+ * @str: The string to write
+ * @count: Pointer to the character count
+ */
+void _write_str(char *str, int *count)
+{
+	while (*str)
+	{
+		_write_char(*str, count);
+		str++;
+	}
+}
+
+/**
+ * _write_positive_int - Write a positive integer to the standard output
+ * @n: The positive integer to write
+ * @count: Pointer to the character count
+ */
+void _write_positive_int(int n, int *count)
+{
+	if (n == 0)
+		_write_char('0', count);
+	else if (n > 0)
+	{
+		if (n / 10)
+			_write_positive_int(n / 10, count);
+		_write_char(n % 10 + '0', count);
+	}
+}
+
+/**
+ * _write_int - Write an integer to the standard output
+ * @n: The integer to write
+ * @count: Pointer to the character count
+ */
+void _write_int(int n, int *count)
+{
+	if (n < 0)
+	{
+		_write_char('-', count);
+		n = -n;
+	}
+	_write_positive_int(n, count);
+}
 
 /**
  * _printf - Custom printf function
  * @format: The format string
- * @...: Variable arguments
- *
  * Return: The number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int firstchar = 0;
+	int count = 0;
 	va_list mylist;
 
 	if (format == NULL)
@@ -24,8 +78,7 @@ int _printf(const char *format, ...)
 	{
 		if (*format != '%')
 		{
-			write(1, format, 1);
-			firstchar++;
+			_write_char(*format, &count);
 		}
 		else
 		{
@@ -35,24 +88,24 @@ int _printf(const char *format, ...)
 			else if (*format == 'c')
 			{
 				char c = va_arg(mylist, int);
-				write(1, &c, 1);
-				firstchar++;
+				_write_char(c, &count);
 			}
 			else if (*format == 's')
 			{
 				char *str = va_arg(mylist, char *);
-				int str_len = 0;
-
-				while (str[str_len] != '\0')
-					str_len++;
-
-				write(1, str, str_len);
-				firstchar += str_len;
+				if (str == NULL)
+					str = "(null)";
+				_write_str(str, &count);
 			}
-			else if (*format == '%')
+			else if (*format == 'd' || *format == 'i')
 			{
-				write(1, format, 1);
-				firstchar++;
+				int num = va_arg(mylist, int);
+				_write_int(num, &count);
+			}
+			else
+			{
+				_write_char('%', &count);
+				_write_char(*format, &count);
 			}
 		}
 		format++;
@@ -60,6 +113,6 @@ int _printf(const char *format, ...)
 
 	va_end(mylist);
 
-	return firstchar;
+	return (count);
 }
 
